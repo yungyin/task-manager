3e8366974c116bcd1ba4ea5755a0959beba22451
yungyinlo <ying2716@gmail.com>
2024-10-24 23:20:30 +0800

    Refactor mem_store_test.go to use testify for assertions

--- datastore/mem_store_test.go
+++ datastore/mem_store_test.go
@@ -1,151 +1,88 @@
 package datastore
 
 import (
-	"errors"
-	"reflect"
+	"github.com/stretchr/testify/assert"
 	"task-manager/models"
 	"testing"
 )
 
-func TestNewMemStore(t *testing.T) {
-	memStore := NewMemStore()
+var memStore = MemStore{
+	taskMap: map[string]models.Task{
+		"1": {Id: "1", Name: "Task 1", Status: models.Incomplete},
+		"2": {Id: "2", Name: "Task 2", Status: models.Complete},
+	},
+}
+var inputTask = models.Task{Name: "name", Status: models.Complete}
 
-	if memStore == nil {
-		t.Fatalf("expected non-nil MemStore, but got nil")
-	}
-	if memStore.taskMap == nil {
-		t.Fatalf("expected taskMap to be initialized, but got nil")
-	}
-	if len(memStore.taskMap) != 0 {
-		t.Errorf("expected taskMap to be empty, but got %d elements", len(memStore.taskMap))
-	}
+const nonExistedId = "0"
+const existedId = "1"
+
+func TestNewMemStore(t *testing.T) {
+	store := NewMemStore()
+
+	assert.NotNil(t, store)
+	assert.NotNil(t, store.taskMap)
+	assert.Equal(t, 0, len(store.taskMap))
 }
 
 func TestMemStore_List(t *testing.T) {
-	memStore := MemStore{
-		taskMap: map[string]models.Task{
-			"1": {Id: "1", Name: "Task 1", Status: models.Incomplete},
-			"2": {Id: "2", Name: "Task 2", Status: models.Complete},
-		},
-	}
-	expected := []models.Task{}
-	for _, task := range memStore.taskMap {
-		expected = append(expected, task)
+	expected := []models.Task{
+		{Id: "1", Name: "Task 1", Status: models.Incomplete},
+		{Id: "2", Name: "Task 2", Status: models.Complete},
 	}
 
 	actual, err := memStore.List()
 
-	if err != nil {
-		t.Fatalf("expected no error, but got %v", err)
-	}
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("expected: %v, but got: %v", expected, actual)
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
 }
 
 func TestMemStore_Create(t *testing.T) {
-	memStore := MemStore{
-		taskMap: make(map[string]models.Task),
-	}
-	inputTask := models.Task{Name: "name", Status: models.Incomplete}
-
 	createdTask, err := memStore.Create(inputTask)
 
-	if err != nil {
-		t.Fatalf("expected no error, but got %v", err)
-	}
-	if createdTask.Id == "" {
-		t.Errorf("expected task to have an ID, but got an empty string")
-	}
-	if createdTask.Name != inputTask.Name {
-		t.Errorf("expected task name: %v, but got: %v", inputTask.Name, createdTask.Name)
-	}
-	if createdTask.Status != inputTask.Status {
-		t.Errorf("expected task status: %v, but got: %v", inputTask.Status, createdTask.Status)
-	}
+	assert.NoError(t, err)
+	assert.NotEmpty(t, createdTask.Id)
+	assert.Equal(t, createdTask.Name, inputTask.Name)
+	assert.Equal(t, createdTask.Status, inputTask.Status)
 
 	validateStoredTask(t, memStore, createdTask.Id, inputTask)
 }
 
 func TestMemStore_Update_GivenNonExistedId_ReturnNotFoundError(t *testing.T) {
-	memStore := MemStore{
-		taskMap: make(map[string]models.Task),
-	}
-	inputId := "1"
-	inputTask := models.Task{Name: "name", Status: models.Complete}
+	_, err := memStore.Update(nonExistedId, inputTask)
 
-	_, err := memStore.Update(inputId, inputTask)
-
-	if !errors.Is(err, NotFoundError) {
-		t.Fatalf("expected to be NotFoundError, but got %v", err)
-	}
+	assert.Equal(t, err, NotFoundError)
 }
 
 func TestMemStore_Update_GivenExistedId_UpdateInStoreAndReturnUpdatedTask(t *testing.T) {
-	memStore := MemStore{
-		taskMap: map[string]models.Task{
-			"1": {Id: "1", Name: "Task 1", Status: models.Incomplete},
-			"2": {Id: "2", Name: "Task 2", Status: models.Complete},
-		},
-	}
-	inputId := "1"
-	inputTask := models.Task{Name: "name", Status: models.Complete}
-	expectedTask := models.Task{Id: "1", Name: "name", Status: models.Complete}
+	updatedTask, err := memStore.Update(existedId, inputTask)
 
-	updatedTask, err := memStore.Update(inputId, inputTask)
+	assert.NoError(t, err)
+	assert.Equal(t, existedId, updatedTask.Id)
+	assert.Equal(t, updatedTask.Name, inputTask.Name)
+	assert.Equal(t, updatedTask.Status, inputTask.Status)
 
-	if err != nil {
-		t.Fatalf("expected no error, but got %v", err)
-	}
-	if !reflect.DeepEqual(expectedTask, updatedTask) {
-		t.Errorf("expected: %v, but got: %v", expectedTask, updatedTask)
-	}
-
-	validateStoredTask(t, memStore, inputId, inputTask)
+	validateStoredTask(t, memStore, existedId, inputTask)
 }
 
 func TestMemStore_Delete_GivenNonExistedId_ReturnNotFoundError(t *testing.T) {
-	memStore := MemStore{
-		taskMap: make(map[string]models.Task),
-	}
-	inputId := "1"
+	err := memStore.Delete(nonExistedId)
 
-	err := memStore.Delete(inputId)
-
-	if !errors.Is(err, NotFoundError) {
-		t.Fatalf("expected to be NotFoundError, but got %v", err)
-	}
+	assert.Equal(t, err, NotFoundError)
 }
 
 func TestMemStore_Delete_GivenExistedId_DeleteFromStore(t *testing.T) {
-	memStore := MemStore{
-		taskMap: map[string]models.Task{
-			"1": {Id: "1", Name: "Task 1", Status: models.Incomplete},
-			"2": {Id: "2", Name: "Task 2", Status: models.Complete},
-		},
-	}
-	inputId := "1"
+	err := memStore.Delete(existedId)
+	assert.NoError(t, err)
 
-	err := memStore.Delete(inputId)
-
-	if err != nil {
-		t.Fatalf("expected no error, but got %v", err)
-	}
-
-	_, exists := memStore.taskMap[inputId]
-	if exists {
-		t.Errorf("expected task to be not found, but it was stored in memStore")
-	}
+	_, exists := memStore.taskMap[existedId]
+	assert.False(t, exists)
 }
 
 func validateStoredTask(t *testing.T, memStore MemStore, inputId string, inputTask models.Task) {
 	storedTask, exists := memStore.taskMap[inputId]
-	if !exists {
-		t.Errorf("expected task to be stored in memStore, but it was not found")
-	}
+	assert.True(t, exists)
 
 	expectedTask := models.Task{Id: inputId, Name: inputTask.Name, Status: inputTask.Status}
-	if !reflect.DeepEqual(expectedTask, storedTask) {
-		t.Errorf("expected: %v, but got: %v", expectedTask, storedTask)
-	}
+	assert.Equal(t, expectedTask, storedTask)
 }
